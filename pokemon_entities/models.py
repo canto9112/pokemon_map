5c11b36d2714a7f3c3e076621cbc3c22da6eb06f
canto9112 <cantoweb112@gmail.com>
2021-07-21 15:17:30 +0300

    Добавил в PokemonEntity поля Уровень, Здоровье, Атака, Защита, Выносливость.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -17,6 +17,11 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     appeared_at = models.DateTimeField(blank=True, null=True)
     disappeared_at = models.DateTimeField(blank=True, null=True)
+    level = models.IntegerField(blank=True, null=True)
+    health = models.IntegerField(blank=True, null=True)
+    strength = models.IntegerField(blank=True, null=True)
+    defence = models.IntegerField(blank=True, null=True)
+    stamina = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return str(self.pokemon)
