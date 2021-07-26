8a3c71b583c0cd760abd6a43135fa0dd56dfb695
canto9112 <cantoweb112@gmail.com>
2021-07-26 18:39:39 +0300

    Добавил переводы названий покемонов

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,7 +6,9 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='image/', blank=True, null=True)
-    show_pokemon = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    english_name = models.CharField(max_length=200, blank=True, null=True)
+    japan_name = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.title
