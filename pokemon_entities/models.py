3218a517a76aba9907ef9d2f4f511f151fc19daa
canto9112 <cantoweb112@gmail.com>
2021-07-29 20:28:45 +0300

    Добавил related_name к сущности покемона

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -32,7 +32,10 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
+    pokemon = models.ForeignKey(Pokemon,
+                                on_delete=models.CASCADE,
+                                verbose_name='Покемон',
+                                related_name='entity')
 
     lat = models.FloatField(verbose_name='Долгота')
     lon = models.FloatField(verbose_name='Широта')
